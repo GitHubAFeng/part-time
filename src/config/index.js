6f49ab06067e01695369e336b75e7fa7f3b3678b
GitHubAFeng <283215159@qq.com>
2018-04-24 01:03:40 +0800

    修复BUG

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 
 
-export const BaseURL = 'https://task.iafeng.cc';
-// export const BaseURL = 'http://127.0.0.1:7001';
+export const BaseURL = 'https://task.iafeng.cc';  //生产环境
+// export const BaseURL = 'http://127.0.0.1:7001'; //测试
 
